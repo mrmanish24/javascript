c76e2aea50efcdc5b16b0265195285cbf64db685
Manish Thakur <148072334+mrmanish24@users.noreply.github.com>
2024-08-01 21:50:03 +0530

    array

---
+++ array.js
@@ -0,0 +1,32 @@
+const array = [1,2,3,4,5]
+
+// reference type
+
+// console.log(array.pop())
+// console.log(array)
+
+// array.push(5)
+// console.log(array)
+// array.shift()
+// console.log(array)
+
+// array.unshift(1)
+
+// console.log(array)
+// how to clone a array
+
+
+// const newarray = [].concat(array)
+// console.log(newarray)
+
+// const newarray2 = [...array]
+// console.log(newarray2)
+
+
+// array destructuring
+
+
+// const [one,two, three, ...rem] = array
+
+// console.log(one)
+// console.log(rem)
